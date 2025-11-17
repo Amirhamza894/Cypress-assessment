3b09ef75c77cbc5ea4ed08427c65023b992fab87
amir-hamza9d <amir.hamza@imaginationai.net>
2025-11-17 23:20:27 +0500

    update login tests and add wait

--- cypress/integration/e2e/login.spec.js
+++ cypress/integration/e2e/login.spec.js
@@ -16,28 +16,31 @@ describe('E2E Login Functionality', () => {
   it('should show an error message with an invalid email (negative)', () => {
     loginPage
     .getLoginButton()
+    .should('be.visible', { timeout: 10000 })
     .click();
 
     loginPage
     .getLoginWithEmail()
+    .should('be.visible', { timeout: 10000 })
     .click();
 
     loginPage
     .getEmailInput()
-    .should('be.visible')
+    .should('be.visible', { timeout: 10000 })
     .type(Cypress.env('TEST_USERNAME'));
 
     loginPage
     .getPasswordInput()
-    .should('be.visible')
+    .should('be.visible', { timeout: 10000 })
     .type(Cypress.env('TEST_PASSWORD'));
 
     loginPage
     .getPasswordContinueButton()
+    .should('be.visible', { timeout: 10000 })
     .click();
 
     loginPage
-    .getNoAccountText()
+    .getNoAccountText().should('be.visible', { timeout: 10000 })
     .should('have.text', "No account associated with this email address")
     // cy
     // .get('.alert-danger')
@@ -47,28 +50,32 @@ describe('E2E Login Functionality', () => {
   it('should show an error message with an invalid password (negative)', () => {
     loginPage
     .getLoginButton()
+    .should('be.visible', { timeout: 10000 })
     .click();
 
     loginPage
     .getLoginWithEmail()
+    .should('be.visible', { timeout: 10000 })
     .click();
 
     loginPage
     .getEmailInput()
-    .should('be.visible')
+    .should('be.visible', { timeout: 10000 })
     .type(Cypress.env('VALID_EMAIL'));
 
     loginPage
     .getPasswordInput()
-    .should('be.visible')
+    .should('be.visible', { timeout: 10000 })
     .type(Cypress.env('INVALID_PASS'));
 
     loginPage
     .getPasswordContinueButton()
+    .should('be.visible', { timeout: 10000 })
     .click();
 
     loginPage
     .getInvalidCredentialsMessage()
+    .should('be.visible', { timeout: 10000 })
     .should('have.text', 'Invalid password');
   });
 });
